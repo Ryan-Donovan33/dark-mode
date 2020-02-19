dab42171f7e3f292576969e367754509ec1780eb
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-18 20:55:36 -0500

    Took awhile to remember how to manipulate DOM, found a great article online. document.querySelector. Now have the logic and targeting the class correctly. Should be MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,11 +2,18 @@ import React, { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = () => {
-	const [ darkMode, setDarkMode ] = useLocalStorage(status);
+	const [ darkMode, setDarkMode ] = useLocalStorage('status');
 
-	// useEffect(() => {
-
-	// }
+	useEffect(
+		() => {
+			if (darkMode === true) {
+				document.querySelector('body').classList.add('dark-mode');
+			} else if (darkMode === false) {
+				document.querySelector('body').classList.remove('dark-mode');
+			}
+		},
+		[ darkMode ]
+	);
 
 	return [ darkMode, setDarkMode ];
 };
